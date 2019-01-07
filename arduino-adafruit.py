0ee3b61cab1405282b840736845f6447c8ac5661
Jeremy Poulter <jeremy@bigjungle.net>
2019-01-07 21:31:52 +0000

    Tidy up to be more complete and work toward supporting the 52840

--- arduino-adafruit.py
+++ arduino-adafruit.py
@@ -32,30 +32,16 @@ variant = board.get("build.variant")
 FRAMEWORK_DIR = platform.get_package_dir("framework-arduinoadafruitnordicnrf5")
 assert isdir(FRAMEWORK_DIR)
 
-core_path = join(FRAMEWORK_DIR, "cores", board.get("build.core"))
-rtos_path = join(core_path, "freertos")
-nordic_path = join(core_path, "nordic")
+CORE_DIR = join(FRAMEWORK_DIR, "cores", board.get("build.core"))
+assert isdir(CORE_DIR)
 
-nrf_flags = [
-    core_path,
-    join(core_path, "cmsis", "include"),
-    nordic_path,
-    join(nordic_path, "nrfx"),
-    join(nordic_path, "nrfx", "hal"),
-    join(nordic_path, "nrfx", "mdk"),
-    join(nordic_path, "nrfx", "soc"),
-    join(nordic_path, "nrfx", "drivers", "include"),
-    join(nordic_path, "softdevice", "{0}_nrf52_{1}_API".format(board.get("build.softdevice.sd_name"), board.get("build.softdevice.sd_version")), "include"),
-    join(rtos_path, "Source", "include"),
-    join(rtos_path, "config"),
-    join(rtos_path, "portable", "GCC", "nrf52"),
-    join(rtos_path, "portable", "CMSIS", "nrf52"),
-    join(core_path, "sysview", "SEGGER"),
-    join(core_path, "sysview", "Config"),
-    join(core_path, "usb"),
-    join(core_path, "usb", "tinyusb", "src"),
-    join(core_path, "cmsis", "include")
-]
+NORDIC_DIR = join(CORE_DIR, "nordic")
+assert isdir(NORDIC_DIR)
+
+# IMPROVE: Read all of these (defaults) from build.txt/platform.txt/programmers.txt?
+bsp_version = board.get("build.bsp.version", "0.9.2")
+softdevice_version = board.get("build.softdevice.sd_version", "6.1.1")
+bootloader_version = board.get("build.bootloader.version", "0.2.6")
 
 env.Append(
     ASFLAGS=["-x", "assembler-with-cpp"],
@@ -85,14 +71,45 @@ env.Append(
         ("F_CPU", board.get("build.f_cpu")),
         "ARDUINO_ARCH_NRF5",
         "NRF5",
-        board.get("build.softdevice.sd_name")
+        "NRF52",
+        "ARDUINO_FEATHER52",
+        "ARDUINO_NRF52_ADAFRUIT",
+        "NRF52_SERIES",
+        ("ARDUINO_BSP_VERSION", '\\"%s\\"' % bsp_version )
     ],
 
     LIBPATH=[
-        join(core_path, "linker")
+        join(CORE_DIR, "linker")
     ],
 
-    CPPPATH=nrf_flags,
+    #compiler.nrf.flags= -DARDUINO_FEATHER52 -DARDUINO_NRF52_ADAFRUIT -DNRF52_SERIES {build.flags} {build.debug_flags} 
+    # "-I{build.core.path}/cmsis/include" 
+    # "-I{nordic.path}" 
+    # "-I{nordic.path}/nrfx" 
+    # "-I{nordic.path}/nrfx/hal" 
+    # "-I{nordic.path}/nrfx/mdk" 
+    # "-I{nordic.path}/nrfx/soc" 
+    # "-I{nordic.path}/nrfx/drivers/include" 
+    # "-I{nordic.path}/softdevice/{build.name}_nrf52_{build.version}_API/include" 
+    # "-I{rtos.path}/Source/include" 
+    # "-I{rtos.path}/config" 
+    # "-I{rtos.path}/portable/GCC/nrf52"
+    # "-I{rtos.path}/portable/CMSIS/nrf52" 
+    # "-I{build.core.path}/sysview/SEGGER" 
+    # "-I{build.core.path}/sysview/Config" 
+    # "-I{build.core.path}/usb" 
+    # "-I{build.core.path}/usb/tinyusb/src"
+
+    CPPPATH = [
+        join(CORE_DIR),
+        join(CORE_DIR, "cmsis", "include"),
+        join(NORDIC_DIR),
+        join(NORDIC_DIR, "nrfx"),
+        join(NORDIC_DIR, "nrfx", "hal"),
+        join(NORDIC_DIR, "nrfx", "mdk"),
+        join(NORDIC_DIR, "nrfx", "soc"),
+        join(NORDIC_DIR, "nrfx", "drivers", "include"),
+    ],
 
     LINKFLAGS=[
         "-Os",
@@ -114,18 +131,32 @@ env.Append(
 if "BOARD" in env:
     env.Append(
         CCFLAGS=[
-            "-mcpu=%s" % env.BoardConfig().get("build.cpu")
+            "-mcpu=%s" % board.get("build.cpu")
         ],
         LINKFLAGS=[
-            "-mcpu=%s" % env.BoardConfig().get("build.cpu")
+            "-mcpu=%s" % board.get("build.cpu")
         ]
     )
 
 if board.get("build.cpu") == "cortex-m4":
+    # Add option to use software FP?
+    #env.Append(
+    #    CCFLAGS=[
+    #        "-mfloat-abi=softfp",
+    #        "-mfpu=fpv4-sp-d16"
+    #    ]
+    #)
+
     env.Append(
         CCFLAGS=[
-            "-mfloat-abi=softfp",
-            "-mfpu=fpv4-sp-d16"
+            "-mfloat-abi=hard",
+            "-mfpu=fpv4-sp-d16",
+            "-u _printf_float"
+        ],
+        LINKFLAGS=[
+            "-mfloat-abi=hard",
+            "-mfpu=fpv4-sp-d16",
+            "-u _printf_float"
         ]
     )
 
@@ -136,42 +167,81 @@ env.Append(
 
 # Process softdevice options
 softdevice_name = board.get("build.softdevice.sd_name")
+board_name = board.get("build.bootloader", board.get("build.variant"))
 
 if softdevice_name:
-
     env.Append(
-        CPPDEFINES=["%s" % softdevice_name.upper()]
+        CPPPATH=[
+            join(NORDIC_DIR, "softdevice", "%s_nrf52_%s_API" % (softdevice_name, softdevice_version), "include")
+        ],
+        CPPDEFINES=[
+            "%s" % softdevice_name.upper(),
+            "NRF52_"+(softdevice_name.upper()),
+            "SOFTDEVICE_PRESENT"
+        ]
     )
 
-    hex_path = join(FRAMEWORK_DIR, "bootloader", board.get("build.variant"))
+    hex_path = join(FRAMEWORK_DIR, "bootloader", board_name)
 
     for f in listdir(hex_path):
-        if f == "{0}_bootloader-{1}_{2}_{3}.hex".format(variant, board.get("build.softdevice.version"), board.get("build.softdevice.sd_name"), board.get("build.softdevice.sd_version")):
-            env.Append(SOFTDEVICEHEX=join(hex_path, f))
+        if f == "{0}_bootloader-{1}_{2}_{3}.hex".format(variant, bootloader_version, softdevice_name, softdevice_version):
+            env.Append(DFUBOOTHEX=join(hex_path, f))
 
-    if "SOFTDEVICEHEX" not in env:
+    if "DFUBOOTHEX" not in env:
         print("Warning! Cannot find an appropriate softdevice binary!")
 
     # Update linker script:
-    ldscript_dir = join(core_path, "linker")
+    ldscript_dir = join(CORE_DIR, "linker")
     mcu_family = board.get("build.mcu")
-    ldscript_path = ""
-    for f in listdir(ldscript_dir):
-        if f.startswith(mcu_family) and softdevice_name in f.lower():
-            ldscript_path = join(ldscript_dir, f)
+    ldscript_name = board.get("build.ldscript", "")
 
-    if ldscript_path:
-        env.Replace(LDSCRIPT_PATH=ldscript_path)
+    if ldscript_name:
+        env.Append(LINKFLAGS=[
+            "-L"+ldscript_dir,
+#            "-T"+ldscript_name
+        ])
+        env.Replace(LDSCRIPT_PATH=ldscript_name)
     else:
         print("Warning! Cannot find an appropriate linker script for the "
               "required softdevice!")
 
-cpp_defines = env.Flatten(env.get("CPPDEFINES", []))
+freertos_path = join(CORE_DIR, "freertos")
+if(isdir(freertos_path)):
+    env.Append(
+        CPPPATH=[
+            join(freertos_path, "Source", "include"),
+            join(freertos_path, "config"),
+            join(freertos_path, "portable", "GCC", "nrf52"),
+            join(freertos_path, "portable", "CMSIS", "nrf52")
+        ]
+    )
+
+sysview_path = join(CORE_DIR, "sysview")
+if(isdir(sysview_path)):
+    env.Append(
+        CPPPATH=[
+            join(sysview_path, "SEGGER"),
+            join(sysview_path, "Config")
+        ]
+    )
+
+usb_path = join(CORE_DIR, "usb")
+if(isdir(usb_path)):
+    env.Append(
+        CPPPATH=[
+            join(usb_path),
+            join(usb_path, "tinyusb", "src")
+        ]
+    )
+
+#print env.Dump()
+
+# cpp_defines = env.Flatten(env.get("CPPDEFINES", []))
 
 # Select crystal oscillator as the low frequency source by default
-clock_options = ("USE_LFXO", "USE_LFRC", "USE_LFSYNT")
-if not any(d in clock_options for d in cpp_defines):
-    env.Append(CPPDEFINES=["USE_LFXO"])
+#clock_options = ("USE_LFXO", "USE_LFRC", "USE_LFSYNT")
+#if not any(d in clock_options for d in cpp_defines):
+#    env.Append(CPPDEFINES=["USE_LFXO"])
 
 #
 # Target: Build Core Library
@@ -193,6 +263,6 @@ if "build.variant" in board:
 libs.append(
     env.BuildLibrary(
         join("$BUILD_DIR", "FrameworkArduino"),
-        join(FRAMEWORK_DIR, "cores", board.get("build.core"))))
+        join(CORE_DIR)))
 
 env.Prepend(LIBS=libs)
