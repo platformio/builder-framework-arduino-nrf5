a5ac7d7a42ee6821f69ffcc788a3906fef67027a
Maximilian Gerhardt <maximilian.gerhardt@rub.de>
2021-11-17 15:43:37 +0100

    Update nrf5.py

--- nrf5.py
+++ nrf5.py
@@ -184,15 +184,22 @@ if not any(d in clock_options for d in cpp_defines):
 libs = []
 
 if "build.variant" in board:
-    env.Append(CPPPATH=[
-        join(FRAMEWORK_DIR, "variants", board.get("build.variant"))
-    ])
+    variants_dir = os.path.join(
+        "$PROJECT_DIR", board.get("build.variants_dir")) if board.get(
+            "build.variants_dir", "") else os.path.join(FRAMEWORK_DIR, "variants")
+    env.Append(
+        CPPPATH=[
+            os.path.join(variants_dir, board.get("build.variant"))
+        ],
 
-    libs.append(
-        env.BuildLibrary(
-            join("$BUILD_DIR", "FrameworkArduinoVariant"),
-            join(FRAMEWORK_DIR, "variants",
-                 board.get("build.variant"))))
+        LIBPATH=[
+            os.path.join(variants_dir, board.get("build.variant"))
+        ],
+    )
+    libs.append(env.BuildLibrary(
+        os.path.join("$BUILD_DIR", "FrameworkArduinoVariant"),
+        os.path.join(variants_dir, board.get("build.variant"))
+    ))
 
 libs.append(
     env.BuildLibrary(
